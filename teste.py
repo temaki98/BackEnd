cea4b4a12ed54fb318b37d66451236b6ca3fbbfe
Nathan <nathanlupus@gmail.com>
2026-08-28 09:49:29 -0300

    att 28_08_2

---
+++ teste.py
@@ -0,0 +1,14 @@
+email = "nathanlupus@gmail.com"
+nome = "nathan de almeida"
+
+posicao = email.find("@")
+servidor = email[posicao + 1:]
+print(servidor)
+
+posicao_espaco = nome.find(" ")
+primeiro_nome = nome[:posicao_espaco]
+primeiro_nome = primeiro_nome.capitalize()
+print(primeiro_nome)
+
+mensagem = f"olá! meu nome é {primeiro_nome} e o meu email: {email}."
+print(mensagem)
